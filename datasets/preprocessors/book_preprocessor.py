f1ac90c5104a6f15bf6682e66548d052f78d4af3
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2024-02-19 18:32:01 +0530

    feat: add book description to dataset

--- datasets/preprocessors/book_preprocessor.py
+++ datasets/preprocessors/book_preprocessor.py
@@ -1,5 +1,8 @@
 import csv
+import json
 import os
+
+import requests
 
 directory = 'data/books'
 all_data = []
@@ -17,10 +20,24 @@ for filename in os.listdir(directory):
             data = [[category] + row for row in reader]
             all_data.extend(data)
 
+for d in all_data:
+    try:
+        response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={d[1].replace(' ', '%20')}")
+        data = json.loads(response.text)
+        items = data["items"]
+        for item in items:
+            description = item["volumeInfo"]["description"]
+            d.append(description)
+            print(f"found for {d[1]}")
+            break
+    except:
+        print(f"not found for {d[1]}")
+        all_data.remove(d)
+
 output_file = '../book_dataset.csv'
 with open(output_file, 'w', newline='', encoding='utf-8') as csv_file:
     writer = csv.writer(csv_file)
-    writer.writerow(['Category'] + ['Title'] + ['Author'] + ['Type'] + ['Rating'] + ['Price'])
+    writer.writerow(['Category'] + ['Title'] + ['Author'] + ['Type'] + ['Rating'] + ['Price'] + ['Description'])
     writer.writerows(all_data)
 
 print("CSV file has been successfully created.")
