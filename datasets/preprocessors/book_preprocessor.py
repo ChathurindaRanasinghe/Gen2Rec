4f6b15d11796ba7a6a67cb980e12a376161716fc
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2024-05-15 22:38:04 +0530

    feat: add price extractor

--- datasets/preprocessors/book_preprocessor.py
+++ datasets/preprocessors/book_preprocessor.py
@@ -1,8 +1,5 @@
 import csv
-import json
 import os
-
-import requests
 
 directory = 'data/books'
 all_data = []
@@ -14,6 +11,12 @@ def extract_numeric_rating(rating_str):
             return float(rating_str.split()[0])
     except ValueError:
         return None
+    return None
+
+
+def replace_price_format(price):
+    if price:
+        return price.replace(',', '')
     return None
 
 
@@ -30,19 +33,19 @@ for filename in os.listdir(directory):
             data = [[category] + row for row in reader]
             all_data.extend(data)
 
-for d in all_data:
-    try:
-        response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={d[1].replace(' ', '%20')}")
-        data = json.loads(response.text)
-        items = data["items"]
-        for item in items:
-            description = item["volumeInfo"]["description"]
-            d.append(description)
-            print(f"found for {d[1]}")
-            break
-    except:
-        print(f"not found for {d[1]}")
-        all_data.remove(d)
+# for d in all_data:
+#     try:
+#         response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={d[1].replace(' ', '%20')}")
+#         data = json.loads(response.text)
+#         items = data["items"]
+#         for item in items:
+#             description = item["volumeInfo"]["description"]
+#             d.append(description)
+#             print(f"found for {d[1]}")
+#             break
+#     except:
+#         print(f"not found for {d[1]}")
+#         all_data.remove(d)
 
 output_file = '../book_dataset.csv'
 with open(output_file, 'w', newline='', encoding='utf-8') as csv_file:
@@ -56,6 +59,7 @@ with open(output_file, mode='r', encoding='utf-8') as infile:
 
 for row in rows:
     row['Rating'] = extract_numeric_rating(row['Rating'])
+    row['Price'] = replace_price_format(row['Price'])
 
 with open(output_file, mode='w', newline='', encoding='utf-8') as outfile:
     fieldnames = reader.fieldnames
