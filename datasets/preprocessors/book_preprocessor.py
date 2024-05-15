9d07596a7d94af862b9979e3d32a6e31039a3bf3
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2024-05-16 00:49:48 +0530

    feat: add value availability check

--- datasets/preprocessors/book_preprocessor.py
+++ datasets/preprocessors/book_preprocessor.py
@@ -1,5 +1,8 @@
 import csv
+import json
 import os
+
+import requests
 
 directory = 'data/books'
 all_data = []
@@ -7,17 +10,13 @@ all_data = []
 
 def extract_numeric_rating(rating_str):
     try:
-        if rating_str:
-            return float(rating_str.split()[0])
+        return float(rating_str.split()[0])
     except ValueError:
         return None
-    return None
 
 
 def replace_price_format(price):
-    if price:
-        return price.replace(',', '')
-    return None
+    return price.replace(',', '')
 
 
 if os.path.exists('genres.csv'):
@@ -33,19 +32,19 @@ for filename in os.listdir(directory):
             data = [[category] + row for row in reader]
             all_data.extend(data)
 
-# for d in all_data:
-#     try:
-#         response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={d[1].replace(' ', '%20')}")
-#         data = json.loads(response.text)
-#         items = data["items"]
-#         for item in items:
-#             description = item["volumeInfo"]["description"]
-#             d.append(description)
-#             print(f"found for {d[1]}")
-#             break
-#     except:
-#         print(f"not found for {d[1]}")
-#         all_data.remove(d)
+for d in all_data:
+    try:
+        response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={d[1].replace(' ', '%20')}")
+        data = json.loads(response.text)
+        items = data["items"]
+        for item in items:
+            description = item["volumeInfo"]["description"]
+            d.append(description)
+            print(f"found for {d[1]}")
+            break
+    except:
+        print(f"not found for {d[1]}")
+        all_data.remove(d)
 
 output_file = '../book_dataset.csv'
 with open(output_file, 'w', newline='', encoding='utf-8') as csv_file:
@@ -58,8 +57,15 @@ with open(output_file, mode='r', encoding='utf-8') as infile:
     rows = list(reader)
 
 for row in rows:
-    row['Rating'] = extract_numeric_rating(row['Rating'])
-    row['Price'] = replace_price_format(row['Price'])
+    if row['Rating']:
+        row['Rating'] = extract_numeric_rating(row['Rating'])
+    if row['Price']:
+        row['Price'] = replace_price_format(row['Price'])
+
+for row in rows:
+    if (not row['Category'] or not row['Title'] or not row['Author'] or not row['Type'] or not row['Rating']
+            or not row['Price']):
+        rows.remove(row)
 
 with open(output_file, mode='w', newline='', encoding='utf-8') as outfile:
     fieldnames = reader.fieldnames
