32a4fcef022153dc1967b3a6e49d4312831caa1a
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2024-05-15 23:13:09 +0530

    feat: game preprocessor changed to create json with all data

--- datasets/preprocessors/game_preprocessor.py
+++ datasets/preprocessors/game_preprocessor.py
@@ -1,45 +1,40 @@
-import pandas as pd
+import csv
+import json
 
-df = pd.read_csv("data/steam-200k.csv")
 
-purchased_data = df[df['Purchased or Played'] == 'purchase']
-played_data = df[df['Purchased or Played'] == 'play']
+def preprocess_and_create_json(filename):
+    data = {}
 
-user_purchased_games = {}
-user_played_games = {}
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            user_id = row['User ID']
+            game_name = row['Game Name']
+            behavior = row['Purchased or Played']
+            hours = float(row['Hours if behavior is play, 1.0 if behavior is purchase'])
 
-for _, row in purchased_data.iterrows():
-    user_id = row['User ID']
-    game_name = row['Game Name']
-    if user_id not in user_purchased_games:
-        user_purchased_games[user_id] = [game_name]
-    else:
-        user_purchased_games[user_id].append(game_name)
+            if user_id not in data:
+                data[user_id] = {'played_games': {}, 'purchased_games': []}
 
-for _, row in played_data.iterrows():
-    user_id = row['User ID']
-    game_name = row['Game Name']
-    if user_id not in user_played_games:
-        user_played_games[user_id] = [game_name]
-    else:
-        user_played_games[user_id].append(game_name)
+            if behavior == 'play':
+                if game_name in data[user_id]['played_games']:
+                    data[user_id]['played_games'][game_name] += hours
+                else:
+                    data[user_id]['played_games'][game_name] = hours
+            elif behavior == 'purchase':
+                data[user_id]['purchased_games'].append(game_name)
 
-userwise_data = pd.DataFrame(columns=['User ID', 'Purchased Games', 'Played Games'])
+    return data
 
-for user_id in set(list(user_purchased_games.keys()) + list(user_played_games.keys())):
-    purchased_games = ', '.join(user_purchased_games.get(user_id, []))
-    played_games = ', '.join(user_played_games.get(user_id, []))
-    userwise_data = userwise_data._append(
-        {'User ID': user_id, 'Purchased Games': purchased_games, 'Played Games': played_games}, ignore_index=True)
 
-userwise_data.to_csv("../game_dataset(userwise).csv", index=False)
-print("CSV file has been successfully created.")
+def write_json_to_file(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
 
-purchased_count = purchased_data.groupby('Game Name').size().reset_index(name='Purchased Count')
-play_hours = played_data.groupby('Game Name')[
-    'Hours if behavior is play, 1.0 if behavior is purchase'].sum().reset_index()
-gamewise_data = pd.merge(purchased_count, play_hours, on='Game Name', how='outer').fillna(0)
 
-gamewise_data.rename(columns={'Hours if behavior is play, 1.0 if behavior is purchase': 'Hours Played'}, inplace=True)
-gamewise_data.to_csv("../game_dataset(gamewise).csv", index=False)
-print("CSV file has been successfully created.")
+csv_file = 'data/steam-200k.csv'
+output_file = '../game_dataset.json'
+
+player_data = preprocess_and_create_json(csv_file)
+write_json_to_file(player_data, output_file)
+print("JSON file has been successfully created.")
