88922163430c9d8fe8504103d382a7f052dd576d
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2024-05-21 00:40:14 +0530

    feat: implement recommendation list and chat interface with configs

--- gen2rec/user-interface/client_interface.py
+++ gen2rec/user-interface/client_interface.py
@@ -2,61 +2,61 @@ import argparse
 
 import gradio as gr
 
-parser = argparse.ArgumentParser(description='Launch Client Interface for a Category')
-parser.add_argument('--category', type=str, default='BOOK', help='Category to use in the recommendation')
-parser.add_argument('--server_name', type=str, default='Gen2Rec', help='Server name to host the app')
-parser.add_argument('--server_port', type=int, default=7861, help='Port number to host the app')
+parser = argparse.ArgumentParser(description="Launch Client Interface for a Category")
+parser.add_argument("--category", type=str, required=True, help="Category to use in the recommendation")
+parser.add_argument("--server_port", type=int, default=8001, help="Port number to host the app")
 args = parser.parse_args()
 
 CATEGORY = args.category
+LIST = "List"
+CHAT = "Chat"
+VISIBILITY = {LIST: True, CHAT: True}
 
 
 def chat_response(message, history):
     history = history or []
-    bot_response = f"{message[::-1]}"
-    history.append([message, bot_response])
+    if message:
+        bot_response = f"{message[::-1]}"
+        history.append([message, bot_response])
     return "", history
 
 
-def option1():
-    return "Option 1 selected"
+def change_visibility(visible):
+    VISIBILITY[LIST] = LIST in visible
+    VISIBILITY[CHAT] = CHAT in visible
+    return gr.update(visible=VISIBILITY[LIST]), gr.update(visible=VISIBILITY[CHAT])
 
 
-def option2():
-    return "Option 2 selected"
+css = """
+.card {
+    background-color: gray;
+    padding: 10px;
+    border-radius:10px;
+}
+"""
 
-
-with gr.Blocks() as demo:
-    # gr.Markdown(f"# Gen2Rec")
+with gr.Blocks(css=css) as demo:
     gr.Markdown(f"# {CATEGORY.capitalize()} Recommendation with Gen2Rec")
     with gr.Tabs():
         with gr.TabItem("Recommendations"):
             with gr.Row():
-                with gr.Column(scale=1):
-                    gr.Markdown("recs")
-
-                with gr.Column(scale=1):
-                    with gr.Accordion("See Details"):
-                        gr.Markdown("### Chat App")
-                        chatbot = gr.Chatbot()
-                        msg = gr.Textbox(label="Message")
-                        submit = gr.Button("Send")
-                        submit.click(chat_response, inputs=[msg, chatbot], outputs=[msg, chatbot])
+                list_column = gr.Column(scale=1, visible=True, variant="panel")
+                with list_column:
+                    for c in range(0, 5):
+                        with gr.Row(elem_classes="card"):
+                            with gr.Column():
+                                gr.Markdown("data")
+                                gr.Button(value="link")
+                chat_column = gr.Column(scale=1, visible=True)
+                with chat_column:
+                    chatbot = gr.Chatbot()
+                    message = gr.Textbox(label="Message")
+                    submit = gr.Button("Send")
+                    submit.click(chat_response, inputs=[message, chatbot], outputs=[message, chatbot])
 
         with gr.TabItem("Configurations"):
-            with gr.Tabs():
-                with gr.TabItem("Option 1"):
-                    btn1 = gr.Button("Option 1")
-                    output1 = gr.Textbox(label="Selected Option")
-                    btn1.click(option1, inputs=None, outputs=output1)
-                    gr.Markdown("Content for Option 1")
-                with gr.TabItem("Option 2"):
-                    btn2 = gr.Button("Option 2")
-                    output2 = gr.Textbox(label="Selected Option")
-                    btn2.click(option2, inputs=None, outputs=output2)
-                    gr.Markdown("Content for Option 2")
+            visible = gr.CheckboxGroup([LIST, CHAT], label="Visible recommendation options", value=[LIST, CHAT])
+            visible.change(change_visibility, inputs=visible, outputs=[list_column, chat_column])
 
 if __name__ == "__main__":
-    args.server_name = ""
-    args.server_port = 7861
-    demo.launch(server_name=args.server_name, server_port=args.server_port)
+    demo.launch(server_port=args.server_port)
