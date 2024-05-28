2debe68804d250886acd355e1c803f05d8d74ed4
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2024-05-28 23:21:16 +0530

    feat: add recommendation cards

--- gen2rec/user-interface/client_interface.py
+++ gen2rec/user-interface/client_interface.py
@@ -1,5 +1,4 @@
 import argparse
-import json
 from time import sleep
 
 import gradio as gr
@@ -13,9 +12,15 @@ VISIBILITY = {LIST: True, CHAT: True}
 FIELDS = []
 
 
+def get_fields(recommendation_fields):
+    # return list(json.loads(recommendation_fields).keys())
+    return ["movie", "year", "genres", "average_rating"]
+
+
 def initialize(recommendation_category, recommendation_fields):
-    FIELDS = list(json.loads(recommendation_fields).keys())
+    FIELDS = get_fields(recommendation_fields)
     print(FIELDS)
+
     if recommendation_category != "":
         print(recommendation_category)
 
@@ -23,15 +28,6 @@ def initialize(recommendation_category, recommendation_fields):
     INITIALIZED = True
     return gr.update(interactive=INITIALIZED), gr.update(interactive=INITIALIZED), gr.update(
         value="Successfully initialized"), gr.update(value=f"# {TITLE} - {CATEGORY.capitalize()} Recommendation")
-
-
-def chat_response(message, history):
-    history = history or []
-    if message:
-        bot_response = f"{message[::-1]}"
-        history.append([message, bot_response])
-        sleep(2)
-    return "", history
 
 
 def change_visibility(visible):
@@ -49,13 +45,67 @@ def submit_configurations(system_prompt, user_details, web_search):
     return "Configurations updated" if response else "Error in configuration"
 
 
+def get_recommendation_list():
+    return [
+        {
+            "page_content": "tags: \n",
+            "metadata": {
+                "movie": "Family Guy Presents Stewie Griffin: The Untold Story",
+                "year": "2005",
+                "genres": "Adventure, Animation, Comedy",
+                "average_rating": 3.708333333,
+                "source": 5989,
+                "_id": "622de1e97b2e405b9087981e7e8d4bdd",
+                "_collection_name": "movies"
+            },
+            "type": "Document"
+        },
+        {
+            "page_content": "tags: \n",
+            "metadata": {
+                "movie": "Toy Story 3",
+                "year": "2010",
+                "genres": "Adventure, Animation, Children, Comedy, Fantasy, IMAX",
+                "average_rating": 4.109090909,
+                "source": 7355,
+                "_id": "d9158014658840adab71089128a96dab",
+                "_collection_name": "movies"
+            },
+            "type": "Document"
+        },
+        {
+            "page_content": "tags: \n",
+            "metadata": {
+                "movie": "Comedian",
+                "year": "2002",
+                "genres": "Comedy, Documentary",
+                "average_rating": 3.0,
+                "source": 4013,
+                "_id": "23eb175895ea45dfa7417711984ac697",
+                "_collection_name": "movies"
+            },
+            "type": "Document"
+        }
+    ]
+
+
+def chat_response(message, history):
+    history = history or []
+    if message:
+        bot_response = f"{message[::-1]}"
+        history.append([message, bot_response])
+        sleep(2)
+    return "", history
+
+
 css = """
 footer {
     visibility: hidden
 }
 
 .card {
-    background-color: gray;
+    background-color: rgb(120,120,120);
+    margin: 10px;
     padding: 10px;
     border-radius:10px;
 }
@@ -89,12 +139,21 @@ with gr.Blocks(css=css, title=TITLE) as demo:
             with gr.Row():
                 list_column = gr.Column(scale=1, visible=True, variant="panel")
                 with list_column:
-                    gr.Markdown("Recommendation List")
-                    for c in range(0, 5):
-                        with gr.Row(elem_classes="card"):
-                            with gr.Column():
-                                gr.Markdown("data")
-                                gr.Button(value="link")
+                    def update_recommendations():
+                        recommendations_html = ""
+                        for rec in get_recommendation_list():
+                            recommendations_html += f'''
+                            <div class="card">
+                            {rec}
+                            </div>
+                            '''
+                        return gr.update(value=recommendations_html)
+
+
+                    refresh = gr.Button("Recommendation List")
+                    recommendation_list = gr.HTML()
+                    refresh.click(update_recommendations, outputs=recommendation_list)
+
                 chat_column = gr.Column(scale=1, visible=True)
                 with chat_column:
                     gr.Markdown("Recommendation Chat Interface")
