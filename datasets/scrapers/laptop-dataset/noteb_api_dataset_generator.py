334a396f59c810a26e598df2b469516e9e380305
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2024-05-29 18:01:42 +0530

    refactor: add global variables and refactor

--- datasets/scrapers/laptop-dataset/noteb_api_dataset_generator.py
+++ datasets/scrapers/laptop-dataset/noteb_api_dataset_generator.py
@@ -2,11 +2,12 @@ import json
 
 import requests
 
+API_URL = "https://noteb.com/api/webservice.php"
+API_KEY = "112233aabbcc"
+
 
 def get_ids() -> list:
-    response = requests.post("https://noteb.com/api/webservice.php", data={"apikey": "112233aabbcc",
-                                                                           "method": "list_models",
-                                                                           "param[model_id]": ""})
+    response = requests.post(API_URL, data={"apikey": API_KEY, "method": "list_models", "param[model_id]": ""})
     parsed_data = response.json()
     return [model["id"] for key in parsed_data["result"] for model in parsed_data["result"][key]["model_info"]]
 
@@ -14,9 +15,8 @@ def get_ids() -> list:
 def get_data(id_list) -> list:
     data = []
     for i in id_list:
-        details = requests.post("https://noteb.com/api/webservice.php", data={"apikey": "112233aabbcc",
-                                                                              "method": "get_model_info",
-                                                                              "param[model_id]": i})
+        details = requests.post(API_URL,
+                                data={"apikey": API_KEY, "method": "get_model_info", "param[model_id]": i})
         parsed_details = details.json()
         if parsed_details.get("result") and len(parsed_details["result"]) > 0:
             print(f"Got data for {i}")
@@ -24,14 +24,16 @@ def get_data(id_list) -> list:
     return data
 
 
-def save_to_file(data):
-    json_file_path = "data/collected_data.json"
-
+def save_to_file(data, json_file_path):
     with open(json_file_path, "w") as json_file:
         json.dump(data, json_file)
 
 
 if __name__ == "__main__":
+    output_file = "data/collected_data.json"
+
     id_list = get_ids()
     data = get_data(id_list)
-    save_to_file(data)
+    save_to_file(data, output_file)
+
+    print("Data gathering completed.")
