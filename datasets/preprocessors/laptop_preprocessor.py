4ed94acf41fe060898e71c1221140df5db278b44
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2024-05-29 13:56:31 +0530

    feat: process noteb laptop dataset

--- datasets/preprocessors/laptop_preprocessor.py
+++ datasets/preprocessors/laptop_preprocessor.py
@@ -1,54 +1,79 @@
 import json
-
-import pandas as pd
-
-json_file = 'data/laptop_scraped.json'
+from datetime import datetime
 
 
-def flatten_json(y):
-    out = {}
-
-    def flatten(x, name=''):
-        if isinstance(x, dict):
-            for a in x:
-                flatten(x[a], name + a + ' ')
-        elif isinstance(x, list):
-            i = 0
-            for a in x:
-                flatten(a, name + str(i) + ' ')
-                i += 1
-        else:
-            out[name.strip()] = x
-
-    flatten(y)
-    return out
+def read_data():
+    json_file = "data/collected_data.json"
+    with open(json_file, 'r') as file:
+        return json.load(file)
 
 
-with open(json_file, 'r') as file:
-    data = json.load(file)
+def remove_duplicates(data):
+    seen = set()
+    unique_data = []
+    for item in data:
+        model_info = item["model_info"][0]
+        identifier = (model_info["name"], model_info["extra_name"], tuple(model_info["submodel_info"]))
+        if identifier not in seen:
+            seen.add(identifier)
+            unique_data.append(item)
+    return unique_data
 
-flattened_data = []
 
-for item in data:
-    flattened_item = flatten_json(item)
-    flattened_data.append(flattened_item)
+def process_json(data):
+    if isinstance(data, dict):
+        return {k: process_json(v) for k, v in data.items() if v is not None and v != ""}
+    elif isinstance(data, list):
+        return [process_json(item) for item in data]
+    elif isinstance(data, str):
+        try:
+            return float(data)
+        except ValueError:
+            return data
+    else:
+        return data
 
-df = pd.DataFrame(flattened_data)
 
-columns_to_check = ['Additional Information Date First Available',
-                    'CPU CPU Name',
-                    'CPU CPU Type',
-                    'Dimensions & Weight Dimensions (W x D x H)',
-                    'Dimensions & Weight Weight',
-                    'Display Screen Size',
-                    'Memory Memory',
-                    'Model Brand',
-                    'Model Model',
-                    'Storage HDD',
-                    'Storage SSD']
+def create_new_dataset(data):
+    new_dataset = []
+    for item in data:
+        try:
+            new_item = {
+                "name": item["model_info"][0]["name"],
+                "thumbnail": item["model_resources"]["thumbnail"],
+                "launch_year": datetime.strptime(item["model_resources"]["launch_date"], "%Y-%m-%d").year,
+                "cpu": process_json(item["cpu"]),
+                "gpu": process_json(item["gpu"]),
+                "display": process_json(item["display"]),
+                "memory": process_json(item["memory"]),
+                "primary_storage": process_json(item["primary_storage"]),
+                "secondary_storage": process_json(item["secondary_storage"]),
+                "total_storage_capacity": process_json(item["total_storage_capacity"]),
+                "wireless_card": process_json(item["wireless_card"]),
+                "motherboard": process_json(item["motherboard"]),
+                "chassis": process_json(item["chassis"]),
+                "battery": process_json(item["battery"]),
+                "battery_life_hours": sum(
+                    float(x) / 60 ** i for i, x in enumerate(item["battery_life_hours"].split(":"))),
+                "operating_system": process_json(item["operating_system"]),
+                "warranty": process_json(item["warranty"]),
+                "price": process_json(item["config_price"]),
+            }
+            new_dataset.append(new_item)
+        except:
+            print(f"Failed to process {item['model_info'][0]['name']}")
 
-df_filtered = df.dropna(subset=columns_to_check, how='any')
+    return new_dataset
 
-df_filtered.to_csv('../laptop_dataset.csv', index=False)
 
-print("CSV file has been successfully created.")
+if __name__ == "__main__":
+    cleaned_data = remove_duplicates(read_data())
+    print("Cleaned dataset size: " + str(len(cleaned_data)))
+
+    processed_data = create_new_dataset(cleaned_data)
+    print("Processed dataset size: " + str(len(processed_data)))
+
+    with open("data/laptop_dataset.json", "w") as file:
+        json.dump(processed_data, file, indent=4)
+
+    print("JSON file has been successfully created.")
