cee3d923dca30a7df9a514af0b3c7f65f42c276d
Dinuka <dinukagayashankasthuriarachchi@gmail.com>
2024-05-29 12:15:02 +0530

    refactor: add data types n refactor

--- datasets/scrapers/laptop-dataset/noteb_api_dataset_generator.py
+++ datasets/scrapers/laptop-dataset/noteb_api_dataset_generator.py
@@ -2,24 +2,36 @@ import json
 
 import requests
 
-response = requests.post("https://noteb.com/api/webservice.php", data={"apikey": "112233aabbcc",
-                                                                       "method": "list_models",
-                                                                       "param[model_id]": ""})
 
-parsed_data = response.json()
+def get_ids() -> list:
+    response = requests.post("https://noteb.com/api/webservice.php", data={"apikey": "112233aabbcc",
+                                                                           "method": "list_models",
+                                                                           "param[model_id]": ""})
+    parsed_data = response.json()
+    return [model["id"] for key in parsed_data["result"] for model in parsed_data["result"][key]["model_info"]]
 
-id_list = [model["id"] for key in parsed_data["result"] for model in parsed_data["result"][key]["model_info"]]
 
-data = []
-for i in id_list:
-    details = requests.post("https://noteb.com/api/webservice.php", data={"apikey": "112233aabbcc",
-                                                                          "method": "get_model_info",
-                                                                          "param[model_id]": i})
-    parsed_details = details.json()
-    if parsed_details.get("result") and len(parsed_details["result"]) > 0:
-        data.append(parsed_details["result"]["0"])
+def get_data(id_list) -> list:
+    data = []
+    for i in id_list:
+        details = requests.post("https://noteb.com/api/webservice.php", data={"apikey": "112233aabbcc",
+                                                                              "method": "get_model_info",
+                                                                              "param[model_id]": i})
+        parsed_details = details.json()
+        if parsed_details.get("result") and len(parsed_details["result"]) > 0:
+            print(f"Got data for {i}")
+            data.append(parsed_details["result"]["0"])
+    return data
 
-json_file_path = "data/collected_data.json"
 
-with open(json_file_path, "w") as json_file:
-    json.dump(data, json_file)
+def save_to_file(data):
+    json_file_path = "data/collected_data.json"
+
+    with open(json_file_path, "w") as json_file:
+        json.dump(data, json_file)
+
+
+if __name__ == "__main__":
+    id_list = get_ids()
+    data = get_data(id_list)
+    save_to_file(data)
