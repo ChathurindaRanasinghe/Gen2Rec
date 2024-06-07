e5e96dbdd9c5d35a0fef3b8fb9ac31232af8106b
chathurinda <chathurindaranasinghe@gmail.com>
2024-06-07 10:08:41 +0530

    Add metadata fields, doc descriptions, format

--- usecases/laptop_ui.py
+++ usecases/laptop_ui.py
@@ -7,6 +7,216 @@ from streamlit_card import card
 
 BACKEND_URL: str = "http://0.0.0.0:8001"
 
+metadata_fields = [
+    {"name": "name", "description": "Name of the laptop.", "type": "string"},
+    {"name": "thumbnail", "description": "Link to the thumbnail.", "type": "string"},
+    {
+        "name": "launch_year",
+        "description": "Launch year of the laptop.",
+        "type": "integer",
+    },
+    {
+        "name": "total_storage_capacity",
+        "description": "Total storage of the laptop in GB(gigabytes).",
+        "type": "integer",
+    },
+    {
+        "name": "battery_life_hours",
+        "description": "Expected battery life of the laptop in hours.",
+        "type": "float",
+    },
+    {
+        "name": "operating_system",
+        "description": "Operating System of the laptop.",
+        "type": "string",
+    },
+    {"name": "price", "description": "Price of the laptop in USD.", "type": "float"},
+    {"name": "cpu_prod", "description": "Producer of the CPU.", "type": "string"},
+    {"name": "cpu_model", "description": "Model of the CPU.", "type": "string"},
+    {
+        "name": "cpu_lithography",
+        "description": "Process lithography of the CPU in nano meters.",
+        "type": "integer",
+    },
+    {"name": "cpu_cache", "description": "Cache of the CPU in MB.", "type": "integer"},
+    {
+        "name": "cpu_base_speed",
+        "description": "Base speed of the CPU in GHz.",
+        "type": "float",
+    },
+    {
+        "name": "cpu_boost_speed",
+        "description": "Boost speed of the CPU in GHz.",
+        "type": "float",
+    },
+    {
+        "name": "cpu_cores",
+        "description": "Number of cores in the CPU.",
+        "type": "integer",
+    },
+    {
+        "name": "cpu_tdp",
+        "description": "Thermal Design Power(TDP) of the CPU.",
+        "type": "integer",
+    },
+    {
+        "name": "cpu_rating",
+        "description": "Defined rating for the CPU.",
+        "type": "float",
+    },
+    {"name": "gpu_prod", "description": "Producer of the GPU.", "type": "string"},
+    {"name": "gpu_model", "description": "Model of the GPU.", "type": "string"},
+    {
+        "name": "gpu_architecture",
+        "description": "Architecture of the GPU.",
+        "type": "string",
+    },
+    {
+        "name": "gpu_lithography",
+        "description": "Process lithography of the GPU in nano meters.",
+        "type": "integer",
+    },
+    {
+        "name": "gpu_shaders",
+        "description": "Number of shaders in the GPU.",
+        "type": "integer",
+    },
+    {
+        "name": "gpu_base_speed",
+        "description": "Base speed of the GPU in MHz.",
+        "type": "integer",
+    },
+    {
+        "name": "gpu_boost_speed",
+        "description": "Boost speed of the GPU in MHz.",
+        "type": "integer",
+    },
+    {
+        "name": "gpu_memory_speed",
+        "description": "Memory speed of the GPU in MHz.",
+        "type": "integer",
+    },
+    {
+        "name": "gpu_memory_bandwidth",
+        "description": "Memory bandwidth of the GPU.",
+        "type": "integer",
+    },
+    {
+        "name": "gpu_memory_size",
+        "description": "Memory size of the GPU in MB.",
+        "type": "integer",
+    },
+    {
+        "name": "gpu_memory_type",
+        "description": "Memory type of the GPU.",
+        "type": "string",
+    },
+    {
+        "name": "gpu_tdp",
+        "description": "Thermal Design Power(TDP) of the GPU.",
+        "type": "integer",
+    },
+    {
+        "name": "gpu_rating",
+        "description": "Defined rating for the GPU.",
+        "type": "float",
+    },
+    {
+        "name": "display_size",
+        "description": "Diagonal display size in inches.",
+        "type": "float",
+    },
+    {
+        "name": "display_horizontal_resolution",
+        "description": "Horizantal display resolution in pixels.",
+        "type": "integer",
+    },
+    {
+        "name": "display_vertical_resolution",
+        "description": "Vertical display resolution in pixels.",
+        "type": "integer",
+    },
+    {
+        "name": "display_type",
+        "description": "Type of the display technology.",
+        "type": "string",
+    },
+    {
+        "name": "memory_size",
+        "description": "Size of the memory in GB.",
+        "type": "integer",
+    },
+    {
+        "name": "memory_speed",
+        "description": "Speed of the memory in MHz.",
+        "type": "integer",
+    },
+    {"name": "memory_type", "description": "Type of the memory.", "type": "string"},
+    {
+        "name": "primary_storage_model",
+        "description": "Primary storage technology.",
+        "type": "string",
+    },
+    {
+        "name": "primary_storage_cap",
+        "description": "Primary storage capacity in GB.",
+        "type": "integer",
+    },
+    {
+        "name": "primary_storage_read_speed",
+        "description": "Primary storage speed in MBps.",
+        "type": "integer",
+    },
+    {
+        "name": "primary_storage_read_speed",
+        "description": "Primary storage speed in MBps.",
+        "type": "integer",
+    },
+    {
+        "name": "wireless_card_model",
+        "description": "Model of the wireless card.",
+        "type": "string",
+    },
+    {
+        "name": "wireless_card_speed",
+        "description": "Speed of the wireless card in Mbps.",
+        "type": "integer",
+    },
+    {
+        "name": "chassis_height_cm",
+        "description": "Height of the laptop chassis in cm.",
+        "type": "float",
+    },
+    {
+        "name": "chassis_depth_cm",
+        "description": "Depth of the laptop chassis in cm.",
+        "type": "float",
+    },
+    {
+        "name": "chassis_width_cm",
+        "description": "Width of the laptop chassis in cm.",
+        "type": "float",
+    },
+    {
+        "name": "chassis_weight_kg",
+        "description": "Weight of the laptop in KG.",
+        "type": "float",
+    },
+    {
+        "name": "battery_capacity",
+        "description": "Capacity of the battery in Wh.",
+        "type": "float",
+    },
+    {
+        "name": "warranty_years",
+        "description": "Information related to the warrenty of the laptop.",
+        "type": "integer",
+    },
+]
+
+
+document_content_description = "Description about the laptop indicating key features, use cases, and types of people that may use the laptops. Description also included some additional information about specs as well."
+
 
 def get_recs():
     return [
@@ -20,9 +230,9 @@ def get_recs():
                 "average_rating": 3.896946565,
                 "source": 939,
                 "_id": "a04c041de83f4fb8ba5db2e983c95ee9",
-                "_collection_name": "movies"
+                "_collection_name": "movies",
             },
-            "type": "Document"
+            "type": "Document",
         },
         {
             "page_content": "tags: action choreography, Horrid characterisation, Poor plot development, sequel, soundtrack\n",
@@ -34,9 +244,9 @@ def get_recs():
                 "average_rating": 3.236842105,
                 "source": 7484,
                 "_id": "9d4554305c2c459a8b83eaabc34512d3",
-                "_collection_name": "movies"
+                "_collection_name": "movies",
             },
-            "type": "Document"
+            "type": "Document",
         },
         {
             "page_content": "tags: \n",
@@ -48,10 +258,10 @@ def get_recs():
                 "average_rating": 3.708333333,
                 "source": 5989,
                 "_id": "622de1e97b2e405b9087981e7e8d4bdd",
-                "_collection_name": "movies"
+                "_collection_name": "movies",
             },
-            "type": "Document"
-        }
+            "type": "Document",
+        },
     ]
 
 
@@ -89,7 +299,7 @@ def recommendation_interface(recs):
                     "margin": "0",
                     "padding": "0",
                 }
-            }
+            },
         )
 
     col1, col2, col3 = st.columns(3)
@@ -120,25 +330,29 @@ def chat_interface() -> None:
         st.chat_message("user").markdown(prompt)
         st.session_state.messages.append({"role": "user", "content": prompt})
 
-        response = get_chat()  # Mocked function to get chat response
-        response_dict = response
-        answer = response_dict["answer"]
+        # response = get_chat()  # Mocked function to get chat response
+        # response_dict = response
+        # answer = response_dict["answer"]
 
         # Display user message
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        st.chat_message("user").markdown(prompt)
+        # st.session_state.messages.append({"role": "user", "content": prompt})
+        # st.chat_message("user").markdown(prompt)
 
         # Simulate streaming assistant response
         assistant_response = ""
         message_placeholder = st.empty()
 
-        for chunk in answer.split():
-            assistant_response += chunk + " "
-            with message_placeholder:
-                st.chat_message("assistant").markdown(assistant_response)
-            time.sleep(0.1)  # Simulate delay for streaming effect
+        with requests.get(
+            url="http://localhost:8000", stream=True, params={"query": prompt}
+        ) as r:
+            for chunk in r.iter_content():
+                assistant_response += chunk + " "
+                with message_placeholder:
+                    st.chat_message("assistant").markdown(assistant_response)
 
-        st.session_state.messages.append({"role": "assistant", "content": assistant_response})
+        st.session_state.messages.append(
+            {"role": "assistant", "content": assistant_response}
+        )
 
 
 if __name__ == "__main__":
