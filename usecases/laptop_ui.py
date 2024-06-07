9a1211733dc2bdcb545d52eec3ac587c1f9a63bb
chathurinda <chathurindaranasinghe@gmail.com>
2024-06-07 10:25:57 +0530

    Add laptop prompt

--- usecases/laptop_ui.py
+++ usecases/laptop_ui.py
@@ -1,5 +1,4 @@
 import json
-import time
 
 import requests
 import streamlit as st
@@ -218,70 +217,36 @@ metadata_fields = [
 document_content_description = "Description about the laptop indicating key features, use cases, and types of people that may use the laptops. Description also included some additional information about specs as well."
 
 
-def get_recs():
-    return [
-        {
-            "page_content": "tags: Action, artificial intelligence, robots, Sci-Fi, special effects, tense, time travel, robots\n",
-            "metadata": {
-                "movie": "Terminator, The",
-                "thumbnail": "https://noteb.com/res/img/models/thumb/t_3733_1.jpg",
-                "year": "1984",
-                "genres": "Action, Sci-Fi, Thriller",
-                "average_rating": 3.896946565,
-                "source": 939,
-                "_id": "a04c041de83f4fb8ba5db2e983c95ee9",
-                "_collection_name": "movies",
-            },
-            "type": "Document",
-        },
-        {
-            "page_content": "tags: action choreography, Horrid characterisation, Poor plot development, sequel, soundtrack\n",
-            "metadata": {
-                "movie": "Tron: Legacy",
-                "year": "2010",
-                "thumbnail": "https://noteb.com/res/img/models/thumb/t_2048_1.jpg",
-                "genres": "Action, Adventure, Sci-Fi, IMAX",
-                "average_rating": 3.236842105,
-                "source": 7484,
-                "_id": "9d4554305c2c459a8b83eaabc34512d3",
-                "_collection_name": "movies",
-            },
-            "type": "Document",
-        },
-        {
-            "page_content": "tags: \n",
-            "metadata": {
-                "movie": "Family Guy Presents Stewie Griffin: The Untold Story",
-                "year": "2005",
-                "thumbnail": "https://noteb.com/res/img/models/thumb/t_2430_1.jpg",
-                "genres": "Adventure, Animation, Comedy",
-                "average_rating": 3.708333333,
-                "source": 5989,
-                "_id": "622de1e97b2e405b9087981e7e8d4bdd",
-                "_collection_name": "movies",
-            },
-            "type": "Document",
-        },
-    ]
+def get_recs(number):
+    body = {"number": number}
+    response = requests.get(
+        BACKEND_URL + "/recommendations",
+        params=body,
+    )
+
+    if response.status_code != 200:
+        raise RuntimeError()
+
+    return response.json()
 
 
-def get_chat():
-    return json.loads("""{
-      "input": "I am interested in action movies can you recommend me 5 moviees",
-      "chat_history": [
-        {
-          "content": "I am interested in action movies can you recommend me 5 moviees",
-          "additional_kwargs": {},
-          "response_metadata": {},
-          "type": "human",
-          "name": null,
-          "id": null,
-          "example": false
-        },
-        "Based on your interest in action movies, here are the recommendations from the given set of movies:\\n\\n1. **The Terminator (1984)**\\n   - **Description:** A relentless cyborg assassin is sent from the future to kill a woman whose unborn son is destined to save humanity from extinction. A human soldier is also sent back to protect her.\\n   - **Rating:** 3.90\\n   - **Genres:** Action, Sci-Fi, Thriller\\n\\n2. **Tron: Legacy (2010)**\\n   - **Description:** The son of a virtual world designer goes looking for his father and ends up inside the digital world that his father designed. He meets his father's corrupted creation and a unique ally who was born inside the digital world.\\n   - **Rating:** 3.24\\n   - **Genres:** Action, Adventure, Sci-Fi, IMAX\\n\\nUnfortunately, I can only recommend 2 action movies from the given set. The other movies do not fall under the action genre."
-      ],
-      "answer": "Based on your interest in action movies, here are the recommendations from the given set of movies:\\n\\n1. **The Terminator (1984)**\\n   - **Description:** A relentless cyborg assassin is sent from the future to kill a woman whose unborn son is destined to save humanity from extinction. A human soldier is also sent back to protect her.\\n   - **Rating:** 3.90\\n   - **Genres:** Action, Sci-Fi, Thriller\\n\\n2. **Tron: Legacy (2010)**\\n   - **Description:** The son of a virtual world designer goes looking for his father and ends up inside the digital world that his father designed. He meets his father's corrupted creation and a unique ally who was born inside the digital world.\\n   - **Rating:** 3.24\\n   - **Genres:** Action, Adventure, Sci-Fi, IMAX\\n\\nUnfortunately, I can only recommend 2 action movies from the given set. The other movies do not fall under the action genre."
-    }""")
+# def get_chat():
+#     return json.loads("""{
+#       "input": "I am interested in action movies can you recommend me 5 moviees",
+#       "chat_history": [
+#         {
+#           "content": "I am interested in action movies can you recommend me 5 moviees",
+#           "additional_kwargs": {},
+#           "response_metadata": {},
+#           "type": "human",
+#           "name": null,
+#           "id": null,
+#           "example": false
+#         },
+#         "Based on your interest in action movies, here are the recommendations from the given set of movies:\\n\\n1. **The Terminator (1984)**\\n   - **Description:** A relentless cyborg assassin is sent from the future to kill a woman whose unborn son is destined to save humanity from extinction. A human soldier is also sent back to protect her.\\n   - **Rating:** 3.90\\n   - **Genres:** Action, Sci-Fi, Thriller\\n\\n2. **Tron: Legacy (2010)**\\n   - **Description:** The son of a virtual world designer goes looking for his father and ends up inside the digital world that his father designed. He meets his father's corrupted creation and a unique ally who was born inside the digital world.\\n   - **Rating:** 3.24\\n   - **Genres:** Action, Adventure, Sci-Fi, IMAX\\n\\nUnfortunately, I can only recommend 2 action movies from the given set. The other movies do not fall under the action genre."
+#       ],
+#       "answer": "Based on your interest in action movies, here are the recommendations from the given set of movies:\\n\\n1. **The Terminator (1984)**\\n   - **Description:** A relentless cyborg assassin is sent from the future to kill a woman whose unborn son is destined to save humanity from extinction. A human soldier is also sent back to protect her.\\n   - **Rating:** 3.90\\n   - **Genres:** Action, Sci-Fi, Thriller\\n\\n2. **Tron: Legacy (2010)**\\n   - **Description:** The son of a virtual world designer goes looking for his father and ends up inside the digital world that his father designed. He meets his father's corrupted creation and a unique ally who was born inside the digital world.\\n   - **Rating:** 3.24\\n   - **Genres:** Action, Adventure, Sci-Fi, IMAX\\n\\nUnfortunately, I can only recommend 2 action movies from the given set. The other movies do not fall under the action genre."
+#     }""")
 
 
 def recommendation_interface(recs):
@@ -326,7 +291,7 @@ def chat_interface() -> None:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    if prompt := st.chat_input(f"Ask for a Movie recommendation"):
+    if prompt := st.chat_input("Ask for a Laptop recommendation"):
         st.chat_message("user").markdown(prompt)
         st.session_state.messages.append({"role": "user", "content": prompt})
 
@@ -356,10 +321,25 @@ def chat_interface() -> None:
 
 
 if __name__ == "__main__":
+    system_prompt = ""
+    with open("laptop_prompt.txt", "r") as f:
+        system_prompt = f.read()
+    body = {
+        "recommendation_category": "laptops",
+        "document_content_description": json.dumps(document_content_description),
+        "metadata_fields": json.dumps(metadata_fields),
+        "system_prompt": system_prompt,
+    }
+    response = requests.post(
+        BACKEND_URL + "/init",
+        data=body,
+    )
+    if response.status_code != 200:
+        raise RuntimeError()
     st.set_page_config(layout="wide")
     st.title("Laptop Shop")
     col1, col2 = st.columns([2, 1])
     with col1:
-        recommendation_interface(get_recs())
+        recommendation_interface(get_recs(4))
     with col2:
         chat_interface()
