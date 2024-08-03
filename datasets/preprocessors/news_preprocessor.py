1bccd52ef72b3473d2b76e9993cc45c274b18c36
Dinuka Gayashan <dinukagayashankasthuriarachchi@gmail.com>
2024-08-03 18:05:20 +0530

    Add dataset preprocessors and collectors (#15)
    * refactor: token_counter update
    
    * docs: add readme
    
    * feat: add news-preprocessor
    
    * feat: add book-preprocessor
    
    * feat: add vehicle-scraper
    
    * docs: readme update
    
    * refactor: restructure the dataset component
    
    * docs: readme update
    
    * refactor: refactor vehicle scraper
    
    * refactor: rename directory
    
    * feat: add vehicle details scraping
    
    * feat: add book description to dataset
    
    * refactor: change config format
    
    * feat: impl movie data processor
    
    * feat: impl game dataset preprocessor
    
    * refactor: rename datasets
    
    * chore: temp save
    
    * feat: vehicle dataset preprocessor impl
    
    * feat: add rating number extraction
    
    * feat: add price extractor
    
    * feat: game preprocessor changed to create json with all data
    
    * feat: add value availability check
    
    * feat: impl laptop data preprocessor with filter
    
    * feat: add year separation
    
    * feat: add empty filteration
    
    * feat: add movie data retrieval with API
    
    * feat: noteb api dataset generator
    
    * feat: add vehicle link scraping with several types
    
    * feat: add failure recovery with data ending
    
    * feat: change write to append
    
    * refactor: add data types n refactor
    
    * feat: process noteb laptop dataset
    
    * refactor: add filenames and refactor preprocessors
    
    * refactor: filename update
    
    * refactor: bug fix, filename update
    
    * refactor: bug fix
    
    * refactor: add data folders to gitignore
    
    * refactor: add global variables and refactor
    
    * refactor: improve efficiency and refactor
    
    * feat: parsing price value
    
    * docs: update dataset details
    
    * feat: add gamewise data preprocessor
    
    * feat: laptop dataset flattening added
    
    * feat: flatten the movie dataset
    
    * refactor: remove unwanted files
    
    * feat: add overview separation
    
    * feat: remove data with empty fields
    
    ---------
    
    Co-authored-by: Chathrinda Ranasinghe <chathurindaranasinghe@gmail.com>

---
+++ datasets/preprocessors/news_preprocessor.py
@@ -0,0 +1,29 @@
+import csv
+import json
+
+
+def convert_json_to_csv(input_file, output_file):
+    with open(input_file, "r") as json_file:
+        json_data = json_file.readlines()
+
+    first_entry = json.loads(json_data[0])
+    field_names = list(first_entry.keys())
+
+    with open(output_file, "w", newline="", encoding="utf-8") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(field_names)
+
+        for line in json_data:
+            data = json.loads(line)
+            if all(data[field] for field in field_names):
+                csv_row = [data[field] for field in field_names]
+                writer.writerow(csv_row)
+
+
+if __name__ == "__main__":
+    input_file = "data/News_Category_Dataset_v3.json"
+    output_file = "data/news_dataset.csv"
+
+    convert_json_to_csv(input_file, output_file)
+
+    print("CSV file has been successfully created.")
